8d98a1aa581c1089969ba92f225acd883b9172cb
Andrew Hammett <hammett@amh.io>
2018-01-20 17:30:35 -0800

    wip, move to async.

--- src/compute.js
+++ src/compute.js
@@ -1,61 +1,140 @@
-'use strict';
+'use strict'
 
+const AWS_DEFAULT_REGION = process.env.AWS_DEFAULT_REGION
+const SUBSCRIPTION_ENDPOINT = process.env.SUBSCRIPTION_ENDPOINT
+const LOG_RETRIEVE_ENDPOINT = process.env.LOG_RETRIEVE_ENDPOINT
+const EMAIL_ENDPOINT = process.env.EMAIL_ENDPOINT
+const max_log_length = 10
+
+const async = require('async')
 const AWS = require('aws-sdk');
 
-const aws_default_region = process.env.AWS_DEFAULT_REGION
-if (!AWS.config.region && aws_default_region) {
+if (!AWS.config.region && AWS_DEFAULT_REGION) {
   AWS.config.update({
-    region: aws_default_region
+    region: AWS_DEFAULT_REGION
   });
 }
 
-const env_name = process.env.ENV_NAME
-const subscription_endpoint = process.env.SUBSCRIPTION_ENDPOINT
-const log_retrieve_endpoint = process.env.LOG_RETRIEVE_ENDPOINT
-const email_stream = process.env.EMAIL_ENDPOINT
-const max_log_length = 10
+let lambda = new AWS.Lambda();
+let kinesis = new AWS.Kinesis();
 
-let generate_jenkins_name = (url) => {
-  let name = url.split('/')[2].split(':')[0].split('.')[0]
-  let replace_tokens = {
-    sm: 'Santa Monica',
-    cimaster: 'CI Master',
-    jenkinsmaster: 'Jenkins Master',
-    localhost: 'Jenkins Local'
+let queue_email = (email_data) => {
+  console.log(email_data)
+  let kinesis_params = {
+    StreamName: EMAIL_ENDPOINT,
+    Data: JSON.stringify(email_data),
+    PartitionKey: 'lambda-event'
   }
-  Object.keys(replace_tokens).forEach(function (key) {
-    name = name.replace(key, replace_tokens[key])
+
+  kinesis.putRecord(kinesis_params, function(error, data) {
+    if(error) {
+      console.error('Error submitting email request: ' + error.message)
+      callback(error)
+    }
+
+    console.log('Submitted request to ' + EMAIL_ENDPOINT)
+    console.log(email_data)
+  })  
+}
+
+let format_email = (subscription_list, email_text, callback) => {
+  let email_data = {
+    from: 'andrew.hammett@sony.com', //generate_reply_email(event_data.build.full_url),
+    to: 'andrew.hammett@sony.com', //subscription.email,
+    subject: 'test email', //generate_subject(event_data),
+    text: email_text
+  }
+  callback(null, email_data)
+}
+
+
+let build_data = (record, subscription_list, callback) => {
+  let payload = JSON.stringify({
+    'build_number': 1, //event_data.build.number,
+    'job_url':      'asdf' //event_data.url
   })
-  return name.replace(/-/g, ' ');
+
+  lambda.invoke({
+    FunctionName: LOG_RETRIEVE_ENDPOINT,
+    Payload: payload
+  }, function(error, data) {
+    if (error) {
+      callback(error)
+    }
+
+    let log_data = JSON.parse(data.Payload)
+    let email_text
+
+    if(log_data.body) {
+      email_text = log_data.body
+    } else {
+      email_text = 'no logs found'
+    }
+    callback(null, subscription_list, email_text)
+  })
 }
 
-let generate_reply_email = (url) => {
-  return generate_jenkins_name(url)+' <no-reply@'+url.split('/')[2].split(':')[0]+'>'
+let subscription_data = (record, callback) => {
+  lambda.invoke({
+    FunctionName: SUBSCRIPTION_ENDPOINT,
+    Payload: ''
+  }, function(error, data) {
+    if (error) {
+      console.log('try')
+      callback(error)
+      return
+    }
+
+    try {
+      let subscription_payload = JSON.parse(data.Payload)
+
+      if(subscription_payload.errorMessage) {
+        console.error('Unable to retrieve subscription data')
+        throw subscription_payload.errorMessage
+      }
+      let subscription_list = JSON.parse(subscription_payload.body)
+
+      console.log('try')
+      callback(null, record, subscription_list)
+
+    } catch(e) {
+      console.log('try:'+ e)
+
+      callback('Error with payload: ' + e)
+    }
+  })
 }
 
-let generate_subject = (data) => {
-  return 'Failure detected in '+data.name+' #'+data.build.number
+module.exports.compute = (event, context, callback) => {
+  async.each(event.Records, function(record, callback) {
+    // this doesn't need to be waterfall ()
+    async.waterfall([
+      async.apply(subscription_data, record),
+      build_data,
+      format_email
+    ], function(error, email_data) {
+      if(error){
+        console.error(error)
+        context.fail(error, '¯\\_(ツ)_/¯')
+      } else {
+        console.log(email_data)
+        queue_email(email_data)
+        context.succeed()
+      }
+    })
+  })
 }
 
-let users_subscribed_to_job = (job_name, pattern) => {
-  let pattern_search = false
-  try {
-    pattern_search = (job_name).search(pattern)
-    pattern_search = true
-  } catch (err) {
-    console.error('invalid search pattern provided')
-  }
-  return pattern_search
-}
+/*
 
 let process_failure_record = (record) => {
   let record_data = new Buffer((record.kinesis.data), 'base64').toString("ascii");
   let kinesis_data = JSON.parse(record_data)
   let event_data = JSON.parse(kinesis_data.body)
 
-  let lambda = new AWS.Lambda();
+  
   lambda.invoke({
-    FunctionName: subscription_endpoint,
+    FunctionName: SUBSCRIPTION_ENDPOINT,
     Payload: ''
   }, function(error, sub_data) {
     if (error) {
@@ -108,7 +187,7 @@ let process_failure_record = (record) => {
               }
 
               let kinesis_params = {
-                StreamName: email_stream,
+                StreamName: EMAIL_ENDPOINT,
                 Data: JSON.stringify(email_data),
                 PartitionKey: 'jfs-lambda-event'
               }
@@ -116,9 +195,9 @@ let process_failure_record = (record) => {
               let kinesis = new AWS.Kinesis();
               kinesis.putRecord(kinesis_params, function(err, data) {
                 if(err) {
-                  console.error('Error submitting email request: '+err.message)
+                  console.error('Error submitting email request: '+ err.message)
                 } else {
-                  console.log('Submitted request to ' + email_stream)
+                  console.log('Submitted request to ' + EMAIL_ENDPOINT)
                   console.log(email_data)
                 }
               })
@@ -129,21 +208,37 @@ let process_failure_record = (record) => {
     }
   });
 }
+*/
 
-module.exports.compute = (event, context, callback) => {
-  if(event.Records) {
-    event.Records.forEach(function(record) {
-      console.log('processing record')
-      process_failure_record(record)
-    });
-  } else {
-    console.error('no data found')
+let generate_jenkins_name = (url) => {
+  let name = url.split('/')[2].split(':')[0].split('.')[0]
+  let replace_tokens = {
+    sm: 'Santa Monica',
+    cimaster: 'CI Master',
+    jenkinsmaster: 'Jenkins Master',
+    localhost: 'Jenkins Local'
   }
+  Object.keys(replace_tokens).forEach(function (key) {
+    name = name.replace(key, replace_tokens[key])
+  })
+  return name.replace(/-/g, ' ');
+}
 
-  callback(null, {
-    statusCode: 200,
-    headers: { 'Content-Type': 'text/plain' },
-    body: 'Greatness awaits...'
-  });
-  return;
+let generate_reply_email = (url) => {
+  return generate_jenkins_name(url)+' <no-reply@'+url.split('/')[2].split(':')[0]+'>'
+}
+
+let generate_subject = (data) => {
+  return 'Failure detected in '+data.name+' #'+data.build.number
+}
+
+let users_subscribed_to_job = (job_name, pattern) => {
+  let pattern_search = false
+  try {
+    pattern_search = (job_name).search(pattern)
+    pattern_search = true
+  } catch (err) {
+    console.error('invalid search pattern provided')
+  }
+  return pattern_search
 }
